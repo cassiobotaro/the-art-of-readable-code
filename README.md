6d9416ddd56b80a34730db9c25d82369f4e69859
cassiobotaro <cassiobotaro@gmail.com>
2025-02-18 21:53:21 -0300

    naive solution

---
+++ README.md
@@ -0,0 +1,18 @@
+# The art of readable code
+
+This repository contains the last chapter code example from the book "The art of readable code" by Dustin Boswell and Trevor Foucher written in Python.
+
+Each directory contains a different example from the book.
+
+## Problem
+
+We need to keep track of how many bytes a web server has transferred over the past minute and over the past hour. Here’s an illustration of how these totals are maintained:
+
+![Two dimensions graph of how many bytes a web server has transferred over the past minute and over the past hour](./problema.png "Two dimensions graph of how many bytes a web server has transferred over the past minute and over the past hour").
+
+## Running the code
+
+```bash
+# to run naive solution
+uv run naive.py
+```
---
+++ naive.py
@@ -0,0 +1,47 @@
+from dataclasses import dataclass
+from time import time as py_time
+
+# Type to represent time in seconds since the Unix epoch (January 1, 1970).
+type t_time = int
+
+
+def time() -> t_time:
+    return int(py_time())
+
+
+class MinuteHourCounter:
+    """Track the cumulative counts over the past minute and over the past hour.
+
+    Useful, for example, to track recent bandwidth usage."""
+
+    @dataclass
+    class Event:
+        count: int
+        time: t_time
+
+    def __init__(self):
+        self.events: list[MinuteHourCounter.Event] = []
+
+    def add(self, count: int):
+        """
+        Add a new data point (count >= 0).
+        For the next minute, MinuteCount() will be larger by +count.
+        For the next hour, HourCount() will be larger by +count.
+        """
+        self.events.append(self.Event(count, time()))
+
+    def _count_since(self, cuttoff: t_time) -> int:
+        count = 0
+        for event in reversed(self.events):
+            if event.time <= cuttoff:
+                break
+            count += event.count
+        return count
+
+    def minute_count(self) -> int:
+        """Return the accumulated count over the past 60 seconds."""
+        return self._count_since(time() - 60)
+
+    def hour_count(self) -> int:
+        """Return the accumulated count over the past 3600 seconds."""
+        return self._count_since(time() - 3600)
